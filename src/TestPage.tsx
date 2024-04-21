d57c4041e32fb2066c2d6d5f8f73aeb3682e1699
Nicolas6272 <49305223+Nicolas6272@users.noreply.github.com>
2024-04-21 18:40:56 +0200

    feat : init

---
+++ src/TestPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Text, View } from 'react-native';
+
+const TestPage: FC = () => {
+  return (
+    <View>
+      <Text>Test Page</Text>
+    </View>
+  );
+};
+
+export default TestPage;
