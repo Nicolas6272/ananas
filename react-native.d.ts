e06fd95d74c5ad4f495db81888cbf84ae2234a9e
Nicolas6272 <49305223+Nicolas6272@users.noreply.github.com>
2024-07-31 16:08:07 +0200

    add env expo public var

--- react-native.d.ts
+++ react-native.d.ts
@@ -1,12 +1,2 @@
 /// ACCEPT classname props for native components
 /// <reference types="nativewind/types" />
-declare module "@env" {
-  export const FIREBASE_API_KEY: string;
-  export const FIREBASE_APP_ID: string;
-  export const FIREBASE_AUTH_DOMAIN: string;
-  export const FIREBASE_MEASUREMENT_ID: string;
-  export const FIREBASE_MESSAGING_SENDER_ID: string;
-  export const FIREBASE_PROJECT_ID: string;
-  export const FIREBASE_STOCKAGE_BUCKET: string;
-}
-
