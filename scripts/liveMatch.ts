24fd52ba5eb1054ff9fcd087888acf9268c45b4f
Nicolas6272 <49305223+Nicolas6272@users.noreply.github.com>
2024-09-14 18:40:09 +0200

    wip

--- scripts/liveMatch.ts
+++ scripts/liveMatch.ts
@@ -1,108 +1,177 @@
 import axios from "axios";
 import { deleteApp } from "firebase/app";
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-  query,
-  setDoc,
-  where,
-} from "firebase/firestore";
-import { Match } from "~/types";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
-const fetchLiveTennisEvents = async (): Promise<Match[]> => {
+const fetchLiveTennisTournamentsIds = async () => {
   const options = {
     method: "GET",
-    url: "https://allsportsapi2.p.rapidapi.com/api/tennis/events/live",
+    url: "https://allsportsapi2.p.rapidapi.com/api/tennis/tournament/all/category/3", ///FOR ATP TOURNAMENTS
     headers: {
-      "x-rapidapi-host": "allsportsapi2.p.rapidapi.com",
       "x-rapidapi-key": "0285a688b9mshffca91e7709c68fp193cfbjsn1a821f7fe710",
+      "x-rapidapi-host": "allsportsapi2.p.rapidapi.com",
     },
   };
 
   try {
     const response = await axios.request(options);
-    const events = response.data.events;
-    const liveMatch: Match[] = events.map((event: any) => {
-      const player1 = event.awayTeam.name;
-      const player1Score = event.awayScore;
-      const player2 = event.homeTeam.name;
-      const player2Score = event.homeScore;
-      return { player1, player1Score, player2, player2Score } as Match;
-    });
-    return liveMatch;
+    return response.data.activeUniqueTournamentIds;
   } catch (error) {
     console.error(error);
-    return [];
   }
 };
 
-const addOrUpdateMatch = async (match: Match) => {
-  const matchesCollection = collection(db, "match");
+const fetchTounamentInfo = async (tournamentId: number) => {
+  console.log("🚀 ~ fetchTounamentInfo ~ tournamentId:", tournamentId);
+  const options = {
+    method: "GET",
+    url: `https://allsportsapi2.p.rapidapi.com/api/tennis/tournament/${tournamentId}/info`,
+    headers: {
+      "x-rapidapi-key": "0285a688b9mshffca91e7709c68fp193cfbjsn1a821f7fe710",
+      "x-rapidapi-host": "allsportsapi2.p.rapidapi.com",
+    },
+  };
 
-  // Create a query against the collection
-  const q = query(
-    matchesCollection,
-    where("player1", "==", match.player1),
-    where("player2", "==", match.player2),
-  );
+  try {
+    const response = await axios.request(options);
+    return response.data.meta;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
+const setTournament = async (tournamentId: number): Promise<void> => {
+  console.log("🚀 ~ setTournament ~ tournamentId:", tournamentId);
+
+  const tournamentCollection = collection(db, "tournaments");
+  const q = query(tournamentCollection, where("id", "==", tournamentId));
+  const querySnapshot = await getDocs(q);
+
+  if (!querySnapshot.empty) {
+    console.log("Tournament with this ID already exists. No action taken.");
+    return;
+  }
+
+  const { surfaceType, cityName, uniqueTournament, numberOfTeams, gender } =
+    await fetchTounamentInfo(tournamentId);
+  const { slug, tennisPoints } = uniqueTournament;
+
+  if (slug.includes("doubles")) {
+    console.log("Tournament is a doubles event. No action taken.");
+    return;
+  }
+
+  const surface = surfaceType.includes("Hard")
+    ? "hard"
+    : surfaceType.includes("Clay")
+      ? "clay"
+      : "grass";
+
+  const level = tennisPoints.toString().includes("2000")
+    ? "grand slam"
+    : tennisPoints.toString().includes("1000")
+      ? "1000"
+      : tennisPoints.toString().includes("500")
+        ? "500"
+        : "250";
+
+  const playerSexe = gender === "M" ? "men" : "female";
+
+  try {
+    await addDoc(tournamentCollection, {
+      id: tournamentId,
+      city: cityName,
+      surface,
+      level,
+      nbOfPlayer: numberOfTeams,
+      playerSexe,
+    });
+    console.log("New tournament created successfully.");
+  } catch (error) {
+    console.error("Error creating tournament:", error);
+  }
+};
+
+const setLiveTournament = async () => {
+  try {
+    const tournamentIds = await fetchLiveTennisTournamentsIds();
+    for (const tournamentId of tournamentIds) {
+      await setTournament(tournamentId);
+    }
+    console.log("Tournament operation completed successfully.");
+  } catch (error) {
+    console.error("Error in main:", error);
+  } finally {
+    await deleteApp(db.app);
+    console.log("App deleted successfully.");
+  }
+};
+
+const setEventToTournament = async (tournamentId: number, matchData: any) => {
+  /// test if match tournament is set in the database
+  const tournamentCollection = collection(db, "tournaments");
+  const q = query(tournamentCollection, where("id", "==", tournamentId));
   const querySnapshot = await getDocs(q);
   if (querySnapshot.empty) {
-    // No matching documents, add new match
-    const newMatchRef = doc(matchesCollection);
-    try {
-      await setDoc(newMatchRef, match);
-      console.log(
-        `Match between ${match.player1} and ${match.player2} added successfully!`,
-      );
-    } catch (error) {
-      console.error("Error adding match: ", error);
-    }
-  } else {
-    // Matching document found, update it
-    querySnapshot.forEach(async doc => {
-      try {
-        console.log("Updating match: ", doc.id);
-        await setDoc(doc.ref, match, { merge: true });
-        console.log(
-          `Match between ${match.player1} and ${match.player2} updated successfully!`,
-        );
-      } catch (error) {
-        console.error("Error updating match: ", error);
-      }
-    });
-  }
-};
-
-const addMatchesToFirestore = async () => {
-  const liveMatch = await fetchLiveTennisEvents();
-  for (const match of liveMatch) {
-    await addOrUpdateMatch(match);
-  }
-
-  console.log("All matches processed successfully!");
-  try {
+    console.log("Tournament with this ID does not exist. No action taken.");
     await deleteApp(db.app);
+  }
+
+  ///test if match already exist in the database
+  const matchCollection = collection(querySnapshot.docs[0].ref, "matches");
+  const q2 = query(matchCollection, where("id", "==", matchData.id));
+  const querySnapshot2 = await getDocs(q2);
+  if (!querySnapshot2.empty) {
+    console.log("Match with this ID already exists.");
+    await querySnapshot2[0].ref.update(matchData);
+    await deleteApp(db.app);
+    return;
+  }
+
+  /// add match to the tournament
+  try {
+    await addDoc(matchCollection, matchData);
+    console.log("Match added successfully.");
   } catch (error) {
-    console.error("Error deleting app: ", error);
+    console.error("Error adding match:", error);
+  } finally {
+    await deleteApp(db.app);
+    console.log("App deleted successfully.");
   }
 };
 
-const deleteAllMatches = async () => {
-  const matchesCollection = collection(db, "match");
-  const querySnapshot = await getDocs(matchesCollection);
-  querySnapshot.forEach(async doc => {
-    try {
-      await deleteDoc(doc.ref);
-      console.log("Match deleted successfully!");
-    } catch (error) {
-      console.error("Error deleting match: ", error);
-    }
-  });
+////////// EVENT //////
+
+const main2 = async () => {
+  // const today = new Date();
+  // const year = today.getFullYear();
+  // const month = today.getMonth() + 1;
+  // const day = today.getDate();
+  // const date = `${day}/${month}/${year}`;
+  // const options = {
+  //   method: "GET",
+  //   url: `https://allsportsapi2.p.rapidapi.com/api/tennis/category/6/events/${date}`,
+  //   headers: {
+  //     "x-rapidapi-key": "0285a688b9mshffca91e7709c68fp193cfbjsn1a821f7fe710",
+  //     "x-rapidapi-host": "allsportsapi2.p.rapidapi.com",
+  //   },
+  // };
+  // try {
+  //   const response = await axios.request(options);
+  //   console.log(response.data);
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
 
-addMatchesToFirestore();
-// deleteAllMatches();
+// main2();
+// setLiveTournament();
+// setEventToTournament(2449, {
+//   id: 2342,
+//   teamA: "Nadal R.",
+//   teamB: "Djokovic N.",
+//   scoreA: 2,
+//   scoreB: 1,
+// });
+
+console.log("Hello world");
